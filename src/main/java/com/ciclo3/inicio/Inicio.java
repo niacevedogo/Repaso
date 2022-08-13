f897984a203aa94953f2161ed332bb73c4662ecb
Nicolas.Acevedo <niacevedogo@unal.edu.co>
2022-08-13 09:44:42 -0500

    segundo comentario

--- src/main/java/com/ciclo3/inicio/Inicio.java
+++ src/main/java/com/ciclo3/inicio/Inicio.java
@@ -3,5 +3,6 @@ package com.ciclo3.inicio;
 public class Inicio {
     public static void main (String[]args){
         System.out.println("Hola Developers");
+        System.out.println("Hola ");
     }
 }
